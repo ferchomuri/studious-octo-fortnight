59b869fceffda86146f9b90db6c602d451e2fc04
Fernando Murillo <edwinmo_1994@hotmail.com>
2026-02-07 18:29:21 -0500

    🎉 Initialize Next.js project with empty home and enable Turbo dev mode.

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className='flex items-center justify-center p-24'>
+      <span className='text-5xl'>Acerca de</span>
+    </div>
+  );
+}
