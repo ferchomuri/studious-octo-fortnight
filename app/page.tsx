4eb82ff6867441aa6bb36c06000e5e36277fbe2f
Fernando Murillo <edwinmo_1994@hotmail.com>
2026-02-07 19:52:26 -0500

    💄 Add shadcn/ui, add global styles, and implement a basic Button component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button variant="default">Click me</Button>
+    </>
   );
 }
