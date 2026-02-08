02c36af5071b3c527a3bcbf0018289d7898dcb86
Fernando Murillo <edwinmo_1994@hotmail.com>
2026-02-08 01:56:44 -0500

    🗑️Clean unnecessary code

--- app/about/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function AboutPage() {
-  return (
-    <div className='flex items-center justify-center p-24'>
-      <span className='text-5xl'>Acerca de</span>
-    </div>
-  );
-}
