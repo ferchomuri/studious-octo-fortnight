de1a322482015451f9cfc57359a4fb114b8a9932
Fernando Murillo <edwinmo_1994@hotmail.com>
2026-02-09 01:03:01 -0500

    🏷️ Add informations home, charts

--- src/lib/interfaces/types.ts
+++ src/lib/interfaces/types.ts
@@ -1,24 +1,40 @@
 export interface MusicRelease {
-  id: string
-  title: string
-  coverImageUrl: string
-  releaseDate: string
-  type: "single" | "ep" | "album"
+  id: string;
+  title: string;
+  coverImageUrl: string;
+  releaseDate: string;
+  type: "single" | "ep" | "album";
 }
 
 export interface SalesAnalytics {
-  period: string
-  value: number
+  period: string;
+  value: number;
 }
 
 export interface FanEngagement {
-  label: string
-  value: number
+  label: string;
+  value: number;
 }
 
 export interface HomeData {
-  totalStreams: number
-  monthlyListeners: number
-  totalRevenue: number
-  engagementRate: number
+  totalStreams: number;
+  monthlyListeners: number;
+  totalRevenue: number;
+  engagementRate: number;
+}
+
+export interface TotalSalesData {
+  label: string;
+  value: number;
+  goal: number;
+}
+
+export interface SalesPerDay {
+  date: string;
+  sales: number;
+}
+
+export interface ReleasesPerStreams {
+  title: string;
+  streams: number;
 }
